64a919d5a69d63cbdb58166c499dd035bd15c0d3
Mindaugas Ulonas <mindegi15@gmail.com>
2023-06-26 06:04:11 +0300

    adding Outlet and Navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Home from './routes/home/home.component';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 const App = () => {
 
+  const Navigation = () => {
+    return (
+      <div>
+        <div>
+          <h1>I'm navigation bar</h1>
+        </div>
+        <Outlet />
+      </div>
+    )
+  }
+
   return (
     <Routes>
-      <Route path='/home' element={<Home />}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/>
+      </Route>
     </Routes>
   );
 }
