1aa6b9e6dc3c1c171cb8ab382657d334312b7e47
MiUlon <mindegi@gmail.com>
2023-10-21 12:18:09 +0300

    implemented function to count items quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,13 +4,13 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
     const isCartOpenToogle = () => setIsCartOpen(!isCartOpen);
 
     return (
         <div className='cart-icon-container'>
             <ShoppingIcon className='shopping-icon' onClick={isCartOpenToogle}/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     );
 };
