e8c82a8e07ca3edb5bf69679af6c862530e8b4bb
MiUlon <mindegi@gmail.com>
2023-10-30 11:40:48 +0200

    implemeneted decreasing function

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,7 +3,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './checkout.styles.scss';
 
 const Checkout = () => {
-    const { cartItems, addItemToCart } = useContext(CartContext);
+    const { cartItems, addItemToCart, removeItemToCart } = useContext(CartContext);
     
     return (
         <div>
@@ -17,7 +17,7 @@ const Checkout = () => {
                             <h2>{name}</h2>
                             <span>{quantity}</span>
                             <br />
-                            <span>Decrement</span>
+                            <span onClick={() => removeItemToCart(cartItem)}>Decrement</span>
                             <br />
                             <span onClick={() => addItemToCart(cartItem)}>Increment</span>
                         </div>
