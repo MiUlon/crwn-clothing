e99c5762189ab26509809c344126f4090a095c64
MiUlon <mindegi@gmail.com>
2023-12-13 13:49:51 +0200

    implemented Styled API

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
-import './sign-in-form.styles.scss';
+import { SignInContainer, Title, ButtonsContainer } from './sign-in-form.styles.jsx';
 
 const defaultFormFields = {
     email: '',
@@ -49,20 +49,20 @@ const SignInForm = () => {
     };
 
     return (
-        <div className='sign-up-container'>
-            <h2>Already have an account?</h2>
+        <SignInContainer>
+            <Title>Already have an account?</Title>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label='Email' type='email' required onChange={handleChange} name='email' value={email}/>
 
                 <FormInput label='Password' type='password' required onChange={handleChange} name='password' value={password}/>
 
-                <div className='buttons-container'>
+                <ButtonsContainer>
                     <Button type='submit'>Sign In</Button>
                     <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
-                </div>
+                </ButtonsContainer>
             </form>
-        </div>
+        </SignInContainer>
     )
 }
 
