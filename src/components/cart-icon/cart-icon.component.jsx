b6c9e80477e0d0a90cac7fe660616f7bc71c08e3
MiUlon <mindegi@gmail.com>
2023-12-29 13:16:20 +0200

    applying Styled-Components for Directory-Item and Cart-Item compoennts

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,17 +1,16 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import './cart-icon.styles.scss';
+import { ShoppingIcon, CartIconContainer, ItemCount } from './cart-icon.styles.jsx';
 
 const CartIcon = () => {
     const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
     const isCartOpenToogle = () => setIsCartOpen(!isCartOpen);
 
     return (
-        <div className='cart-icon-container'>
-            <ShoppingIcon className='shopping-icon' onClick={isCartOpenToogle}/>
-            <span className='item-count'>{cartCount}</span>
-        </div>
+        <CartIconContainer>
+            <ShoppingIcon onClick={isCartOpenToogle}/>
+            <ItemCount>{cartCount}</ItemCount>
+        </CartIconContainer>
     );
 };
 
